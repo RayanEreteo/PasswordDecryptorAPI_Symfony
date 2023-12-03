d57d5dd7464d79d70ec2bcbadd65f436a67e0d2a
RayanEreteo <rayabf5@gmail.com>
2023-12-03 18:06:01 +0100

    Controller terminée

--- src/Controller/PassController.php
+++ src/Controller/PassController.php
@@ -4,16 +4,33 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PassController extends AbstractController
 {
-    #[Route('/pass', name: 'app_pass')]
-    public function index(): JsonResponse
+    #[Route('/encryptPass', name: 'pass_encrypt', methods: ['POST'])]
+    public function encrypt(Request $request): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PassController.php',
-        ]);
+        $plainPass = $request->get('plainPass');
+
+        if ($plainPass === null) {
+            return new JsonResponse(null, JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        $encryptedPass = password_hash($plainPass, PASSWORD_BCRYPT);
+
+        return new JsonResponse(['Hash'=> $encryptedPass, JsonResponse::HTTP_OK]);
+    }
+
+    #[Route('/decryptPass', name: 'pass_decrypt', methods: ['POST'])]
+    public function decrypt(Request $request): JsonResponse
+    {
+        $plainPass = $request->get('plainPass');
+        $hash = $request->get('hash');
+
+        $decryptedPass = password_verify($plainPass, $hash);
+
+        return new JsonResponse(['hashGood'=> $decryptedPass]);
     }
 }
